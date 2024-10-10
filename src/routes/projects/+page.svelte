e06188725b93e3a7fd74fea1ca7d7e125ad966a8
rovic-john-p-de-guia <rovicjohn.p.deguia.08@gmail.com>
2024-10-11 00:56:22 +0800

    fix routing (adding projects)

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,14 @@
+<section>
+    <h1>Projects</h1>
+    <div>
+      <h2>Project 1</h2>
+      <p>Description of your project and technologies used</p>
+      <a href="https://github.com/your-github/repo">GitHub</a>
+    </div>
+    <div>
+      <h2>Project 2</h2>
+      <p>Description of your second project</p>
+      <a href="https://github.com/your-github/repo">GitHub</a>
+    </div>
+  </section>
+  
